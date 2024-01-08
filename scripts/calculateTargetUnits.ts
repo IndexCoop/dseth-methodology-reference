9e3964b307080b4204bb48fb685f4b5361b1399b
christn <c.k.e.koopmann@gmail.com>
2024-01-08 18:12:08 +0800

    Prettier

--- scripts/calculateTargetUnits.ts
+++ scripts/calculateTargetUnits.ts
@@ -1,7 +1,6 @@
 import { ethers } from "hardhat";
 import { MockRatedApi } from "../test/testUtils";
 
-
 import { AuctionRebalanceProposer } from "../src/auctionRebalanceProposer";
 import { eligibleSetTokens } from "../src/addresses";
 import { DEFAULT_AUCTION_CONFIG } from "../src/auctionConfig";
@@ -9,7 +8,6 @@ import { getEnvVars } from "../src/utils";
 import { displayTargetUnits } from "./utils";
 
 async function main() {
-
     let mockedRatedApi = process.env.MOCK_RATED_API == "true";
     if (mockedRatedApi) {
         const mockRatedApi = new MockRatedApi(1, 1);
@@ -19,22 +17,24 @@ async function main() {
 
     console.log("Calculating target units for dsEth");
 
-
     let proposerSigner = await ethers
         .getSigners()
         .then((signers) => signers[0]);
     const { ratedAccessToken, ratedApiUrl } = getEnvVars();
     console.log("Rated API URL: ", mockedRatedApi ? "LOCAL MOCK" : ratedApiUrl);
     let block = await ethers.getDefaultProvider().getBlock("latest");
-    console.log("Current block: ", {number: block.number, timestamp: block.timestamp, hash: block.hash});
-
+    console.log("Current block: ", {
+        number: block.number,
+        timestamp: block.timestamp,
+        hash: block.hash,
+    });
 
     const dsEthProposer = new AuctionRebalanceProposer(
         eligibleSetTokens.dsEth,
         ratedAccessToken,
         ratedApiUrl,
         DEFAULT_AUCTION_CONFIG,
-        proposerSigner
+        proposerSigner,
     );
 
     let targetUnits = await dsEthProposer.getTargetUnits();
