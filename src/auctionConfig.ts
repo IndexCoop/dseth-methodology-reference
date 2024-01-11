d87356fc49f272df27c324c194cb482b68931c5f
Edward Kim <edk217@gmail.com>
2024-01-11 13:36:29 -0500

    fix auction priceConfig calculations

--- src/auctionConfig.ts
+++ src/auctionConfig.ts
@@ -1,16 +1,18 @@
+import { BigNumber, ethers } from "ethers";
+
 export type AuctionConfig = {
     priceAdapterName: string;
     priceAdapterAddress: string;
     quoteAsset: string;
     bucketSize: number;
-    slopeForSellComponents: number;
-    slopeForBuyComponents: number;
+    slopeForSellComponents: BigNumber;
+    slopeForBuyComponents: BigNumber;
     shouldLockSetToken: boolean;
     rebalanceDuration: number;
-    initialPricePctSellComponents: number;
-    initialPricePctBuyComponents: number;
-    maxPriceAsPercentOfMarketPrice: number;
-    minPriceAsPercentOfMarketPrice: number;
+    initialPriceAdjustSellComponents: BigNumber;
+    initialPriceAdjustBuyComponents: BigNumber;
+    maxPriceAboveMarketPrice: BigNumber;
+    minPriceBelowMarketPrice: BigNumber;
 };
 
 export const DEFAULT_AUCTION_CONFIG: AuctionConfig = {
@@ -18,12 +20,12 @@ export const DEFAULT_AUCTION_CONFIG: AuctionConfig = {
     priceAdapterAddress: "0x237F7BBe0b358415bE84AB6d279D4338C0d026bB",
     quoteAsset: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH
     bucketSize: 600, // 10 minutes
-    slopeForSellComponents: 0.00025, // decrease by 0.00025 WETH each bucket
-    slopeForBuyComponents: 0.000015, // increase by 0.00001 WETH each bucket
+    slopeForSellComponents: ethers.utils.parseEther("0.00025"), // decrease by 0.00025 WETH each bucket
+    slopeForBuyComponents: ethers.utils.parseEther("0.0001"), // increase by 0.00001 WETH each bucket
     shouldLockSetToken: false,
     rebalanceDuration: 86400, // 1 days
-    initialPricePctSellComponents: 1.01, // 2% above market price
-    initialPricePctBuyComponents: 0.99, // 2% below market price
-    maxPriceAsPercentOfMarketPrice: 1.01,
-    minPriceAsPercentOfMarketPrice: 0.99,
+    initialPriceAdjustSellComponents: ethers.utils.parseEther("0.01"), // 1% above market price
+    initialPriceAdjustBuyComponents: ethers.utils.parseEther("0.01"), // 1% below market price
+    maxPriceAboveMarketPrice: ethers.utils.parseEther("0.01"),
+    minPriceBelowMarketPrice: ethers.utils.parseEther("0.01"),
 };
